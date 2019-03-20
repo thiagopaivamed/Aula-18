ebb4c8ec74fede0fc8e800150bff274fcc71eff5
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-19 21:03:04 -0300

    Ajax

---
+++ Ajax/Ajax/Models/AjaxDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ajax.Models
+{
+    public class AjaxDB : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public AjaxDB(DbContextOptions<AjaxDB> options) : base(options)
+        {
+
+        }
+    }
+}
